6748068882f1c57a829097dd248ecd2a050be932
Marianela <122845426+MarianelaChiarillo@users.noreply.github.com>
2024-07-01 18:37:22 -0300

    Add files via upload

---
+++ JS/dataJs/dataComentarios.js
@@ -0,0 +1,7 @@
+const comentarios = [
+    { texto: "Excelente servicio, el espacio cumplió con todas nuestras expectativas. Muy recomendado.", autor: "Ana Gómez" },
+    { texto: "El lugar es perfecto para eventos, amplio y bien equipado. El equipo de atención al cliente fue excepcional.", autor: "Pedro Martínez" },
+    { texto: "Alquilamos este espacio para una sesión fotográfica y quedamos encantados. La luz natural era ideal y el ambiente muy acogedor.", autor: "María López" }
+];
+
+export default comentarios;
